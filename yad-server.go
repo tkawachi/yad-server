69cf47fa5239d3dcccdeddd6695ae45d4d05278d
KAWACHI Takashi <tkawachi@gmail.com>
2011-05-19 08:39:33 +0000

    Save tx.counter to redis

--- yad-server.go
+++ yad-server.go
@@ -7,21 +7,26 @@ import (
 	"flag"
 	"strconv"
 	"os"
+	redis "github.com/simonjefford/Go-Redis/src"
+)
+
+const (
+	txCounterKey = "tx.counter"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
+var redisClient redis.Client
 var txChan = make(chan *transactionRequest)
 
 type transaction struct {
-	seq uint64 // TODO serealize seq to metadata store
 }
 
 type transactionRequest struct {
-	c chan uint64 // Write back channel
+	c chan int64 // Write back channel
 }
 
 func newTransactionRequest() *transactionRequest {
-	return &transactionRequest{make(chan uint64)}
+	return &transactionRequest{make(chan int64)}
 }
 
 func newTransaction() *transaction {
@@ -30,13 +35,16 @@ func newTransaction() *transaction {
 
 func (self *transaction) startGen(c chan *transactionRequest) {
 	for {
-		self.seq++
+		seq, err := redisClient.Incr(txCounterKey)
+		if err != nil {
+			log.Fatal("Incr txCounterKey:", err)
+		}
 		req := <-c
-		req.c <- self.seq
+		req.c <- seq
 	}
 }
 
-func getNewTransactionId() uint64 {
+func getNewTransactionId() int64 {
 	txReq := newTransactionRequest()
 	txChan <- txReq
 	txSeq := <-txReq.c
@@ -83,7 +91,7 @@ func storeHandler(w http.ResponseWriter, req *http.Request) {
 	txSeq := getNewTransactionId()
 	log.Println(txSeq)
 	// TODO do actual storing
-	w.Header().Set("X-yad-serial-id", strconv.Uitoa64(txSeq))
+	w.Header().Set("X-yad-serial-id", strconv.Itoa64(txSeq))
 }
 
 func fetchHandler(w http.ResponseWriter, req *http.Request) {
@@ -117,18 +125,29 @@ func moveHandler(w http.ResponseWriter, req *http.Request) {
 	txSeq := getNewTransactionId()
 	log.Println(txSeq)
 	// TODO do actual move
-	w.Header().Set("X-yad-serial-id", strconv.Uitoa64(txSeq))
+	w.Header().Set("X-yad-serial-id", strconv.Itoa64(txSeq))
+}
+
+func connectToRedis() {
+	spec := redis.DefaultSpec()
+	var e os.Error
+	redisClient, e = redis.NewSynchClientWithSpec(spec)
+	if e != nil {
+		log.Fatal("failed to create the client", e)
+		return
+	}
 }
 
 func main() {
 	flag.Parse()
+	connectToRedis()
 	go newTransaction().startGen(txChan)
 	http.Handle("/", http.HandlerFunc(helloHandler))
 	http.Handle("/update", http.HandlerFunc(updateHandler))
 	http.Handle("/store", http.HandlerFunc(storeHandler))
 	http.Handle("/fetch", http.HandlerFunc(fetchHandler))
 	http.Handle("/move", http.HandlerFunc(moveHandler))
-	log.Println(*addr)
+	log.Println(redisClient)
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
